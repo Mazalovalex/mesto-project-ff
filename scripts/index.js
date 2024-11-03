14822abb3d486adba8cd380a94239e36f094c4b1
Mazalov Alex <mazalovalexey1995@yandex.ru>
2024-11-03 13:07:36 +0300

    3 комит

--- scripts/index.js
+++ scripts/index.js
@@ -6,36 +6,28 @@ const placesList = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
 function createCard(cardData) {
-  // Клонируем шаблон карточки
-  const cardFragment = cardTemplate.cloneNode(true); // Клонируем шаблон
-  const cardElement = cardFragment.querySelector(".places__item"); // Получаем элемент карточки
-  console.log("Создана карточка:", cardElement); // Логируем созданную карточку в консоль
+  const cardFragment = cardTemplate.cloneNode(true);
+  const cardElement = cardFragment.querySelector(".places__item");
 
-  // Находим заголовок и изображение
   const cardTitle = cardElement.querySelector(".card__title");
   const cardImage = cardElement.querySelector(".card__image");
 
-  // Устанавливаем заголовок, изображение, текст для альтернативного описания карточки
   cardTitle.textContent = cardData.name;
   cardImage.src = cardData.link;
   cardImage.alt = cardData.name;
 
-  // Находим кнопку удаления
   const deleteButton = cardElement.querySelector(".card__delete-button");
 
-  // Проверяем наличие кнопки и добавляем обработчик клика
   deleteButton.addEventListener("click", function () {
-    deleteCard(cardElement); // Удаляем карточку напрямую
+    deleteCard(cardElement);
   });
 
-  // Возвращаем готовую карточку
   return cardElement;
 }
 
 // @todo: Функция удаления карточки
 function deleteCard(cardElement) {
-  console.log("Удаляем элемент:", cardElement); // Логируем элемент в консоль
-  cardElement.remove(); // Удаляем элемент
+  cardElement.remove();
 }
 
 // @todo: Вывести карточки на страницу
@@ -43,5 +35,3 @@ initialCards.forEach((cardData) => {
   const card = createCard(cardData);
   placesList.append(card);
 });
-
-// Подсказки для меня нужны. Надеюсь вам они не сильно помешали.
